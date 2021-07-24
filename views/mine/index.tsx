57aefde6324fc81aa3a6726a0cfddcc13ed2eea3
rainwildest <rainwildest@163.com>
2021-07-24 18:17:59 +0800

    mine 添加 title

--- views/mine/index.tsx
+++ views/mine/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import { Page } from "framework7-react";
+import { Page, Navbar } from "framework7-react";
 
 const Mine: React.FC = () => {
-  return <Page>mine</Page>;
+  return (
+    <Page>
+      <Navbar title="我的" large titleLarge="我的"></Navbar>
+      <div style={{ height: "1000px" }}></div>
+    </Page>
+  );
 };
 
 export default Mine;
