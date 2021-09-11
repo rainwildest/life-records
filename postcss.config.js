648ff2984478e4715999da072302f039113a9f1c
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2021-09-11 23:45:46 +0800

    处理 0px 编译为 0

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "postcss-pxtorem": {
       rootValue: 16,
       propList: ["*"],
-      unitPrecision: 5
+      unitPrecision: 5,
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: true,
+      minPixelValue: 1
     }
   }
 };
