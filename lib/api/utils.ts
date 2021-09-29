4af7d0a53d82bc5b16b0007bcb96ad286c9d8d10
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-09-29 14:02:25 +0800

    添加百分比

--- lib/api/utils.ts
+++ lib/api/utils.ts
@@ -56,3 +56,75 @@ export const mergeClassName = (
 
   return [...classNameArray].join(" ").replace(/^\s+|\s+$/g, "");
 };
+
+/**
+ * 百分比
+ * @param {number} values 值列表
+ * @param {number} index 索引
+ * @param {number} precision 保留小数，默认为1
+ */
+export const percentage = (
+  values: number[],
+  index: number,
+  precision = 1
+): number => {
+  // 判断是否为空
+  if (!values[index]) return 0;
+
+  // 求和
+  const sum = values.reduce(function (acc, val) {
+    return acc + (isNaN(val) ? 0 : val);
+  }, 0);
+
+  if (sum === 0) return 0;
+
+  // 10的2次幂是100，用于计算精度。
+  const digits = Math.pow(10, precision);
+
+  // 扩大比例100，
+  const votesPerQuota = values.map(function (val) {
+    return ((isNaN(val) ? 0 : val) / sum) * digits * 100;
+  });
+
+  // 总数，扩大比例意味的总数要扩大
+  const targetSeats = digits * 100;
+
+  // 再向下取值，组成数组
+  const seats = votesPerQuota.map(function (votes) {
+    return Math.floor(votes);
+  });
+
+  // 再新计算合计，用于判断与总数量是否相同，相同则占比会100%
+  let currentSum = seats.reduce(function (acc, val) {
+    return acc + val;
+  }, 0);
+
+  // 余数部分的数组：原先数组减去向下取值的数组，得到余数部分的数组
+  const remainder = votesPerQuota.map(function (votes, idx) {
+    return votes - seats[idx];
+  });
+
+  // 给最大最大的余额加1，凑个占比100%；
+
+  while (currentSum < targetSeats) {
+    //  找到下一个最大的余额，给其加1
+    let max = Number.NEGATIVE_INFINITY;
+    let maxId = null;
+    for (let i = 0, len = remainder.length; i < len; ++i) {
+      if (remainder[i] > max) {
+        max = remainder[i];
+        maxId = i;
+      }
+    }
+
+    // 对最大项余额加1
+    ++seats[maxId];
+    // 已经增加最大余数加1，则下次判断就可以不需要再判断这个余额数。
+    remainder[maxId] = 0;
+    // 总的也要加1，为了判断是否总数是否相同，跳出循环。
+    ++currentSum;
+  }
+
+  // 这时候的seats就会总数占比会100%
+  return seats[index] / digits;
+};
