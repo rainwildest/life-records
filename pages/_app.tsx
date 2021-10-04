1c1ee06965952244e2982ec79b86cbc1079da4a8
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2021-10-04 18:45:32 +0800

    修改framework7的使用方式

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,31 @@
-// import App from "next/app";
-// import NextNprogress from "nextjs-progressbar";
 import type { AppProps /* , AppContext */ } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import { useApollo } from "../apollo/client";
 import Framework7 from "framework7/lite-bundle";
-import Framework7React, { App as FrameworkApp } from "framework7-react";
+import Framework7React, { App as FrameworkApp, View } from "framework7-react";
 import f7params from "lib/configure/f7params";
 import fontSizeBase from "lib/fontSizeBase";
 import React, { useEffect } from "react";
+import Router, { useRouter } from "next/router";
+import Head from "next/head";
 import "framework7/framework7-bundle.min.css";
 import "styles/index.scss";
 
-import Head from "next/head";
-
-// Init plugin
 Framework7.use(Framework7React);
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  const apolloClient = useApollo(pageProps.initialApolloState);
+const MyApp = function ({ Component, pageProps }: AppProps): JSX.Element {
+  const apolloClient = useApollo(pageProps?.initialApolloState);
+  // current Next.js route
+  const router = useRouter();
+  /*
+    Here we need to know (mostly on server-side) on what URL user opens our app
+  */
+  const url = `${process.env.NEXT_PUBLIC_HOST}${router.asPath}`;
+
   useEffect(() => {
     if (window) fontSizeBase(window, document);
   }, []);
+
   return (
     <>
       <Head>
@@ -30,10 +35,44 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
         />
       </Head>
       <ApolloProvider client={apolloClient}>
-        <FrameworkApp {...f7params}>
-          <Component {...pageProps} />
+        <FrameworkApp url={url} {...f7params}>
+          <View
+            main
+            browserHistory
+            browserHistorySeparator=""
+            browserHistoryInitialMatch={true}
+            browserHistoryStoreHistory={false}
+            url="/"
+          >
+            <Component initialPage {...pageProps} />
+          </View>
         </FrameworkApp>
       </ApolloProvider>
     </>
   );
-}
+};
+export default MyApp;
+
+MyApp.getInitialProps = async ({ ctx }) => {
+  const asPath = ctx.asPath;
+  const routers = ["/statistics", "/mine"];
+
+  if (routers.includes(asPath)) {
+    if (!ctx.res) return Router.push("/");
+
+    // server
+    ctx.res.writeHead(302, {
+      Location: "/"
+    });
+
+    return ctx.res.end();
+  }
+
+  if (ctx && ctx.req && ctx.req.headers) {
+    return {
+      userAgent: ctx.req.headers["user-agent"]
+    };
+  }
+
+  return {};
+};
