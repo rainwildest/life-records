cd8f18dbf8cf75f0da78c2a28591f4167ad4e94c
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2021-10-10 22:53:59 +0800

    路由

--- lib/routes/module/normalRoutes.ts
+++ lib/routes/module/normalRoutes.ts
@@ -4,26 +4,29 @@ import Statistics from "views/statistics";
 import Mine from "views/mine";
 
 const normalRoutes = [
-  {
-    path: "/",
-    asyncComponent: (): React.ReactNode => import("pages"),
-    tabs: [
-      {
-        path: "/",
-        id: "tab-home",
-        component: Home
-      },
-      {
-        path: "/statistics",
-        id: "tab-statistics",
-        component: Statistics
-      },
-      {
-        path: "/mine",
-        id: "tab-mine",
-        component: Mine
-      }
-    ]
-  }
+  { path: "/home", component: Home },
+  { path: "/statistics", component: Statistics },
+  { path: "/mine", component: Mine }
+  // {
+  //   path: "/",
+  //   asyncComponent: (): React.ReactNode => import("pages"),
+  //   tabs: [
+  //     {
+  //       path: "/",
+  //       id: "tab-home",
+  //       component: Home
+  //     },
+  //     {
+  //       path: "/statistics",
+  //       id: "tab-statistics",
+  //       component: Statistics
+  //     },
+  //     {
+  //       path: "/mine",
+  //       id: "tab-mine",
+  //       component: Mine
+  //     }
+  //   ]
+  // }
 ];
 export default normalRoutes;
