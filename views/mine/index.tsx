d1e6d96f91aeb1687470e42af08730eb72c7e1b1
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-10-20 11:19:49 +0800

    编写功能列表

--- views/mine/index.tsx
+++ views/mine/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, memo } from "react";
+import React, { useState, memo, useEffect } from "react";
 import {
   Page,
   PageContent,
@@ -26,10 +26,14 @@ const Mine: React.FC = () => {
       <PageContent>
         <MineDetails />
 
-        <div className="bg-white p-4 grid gap-2 grid-cols-3 rounded-lg mx-4">
+        <div className="bg-white p-3 grid gap-2 grid-cols-3 rounded-lg mx-4">
           <Link className="block m-0 text-center p-1 rounded-lg">
             <Icons slot="media" name="moon" className="mine-setting-icon" />
             <div className="text-xs">设置</div>
+          </Link>
+          <Link className="block m-0 text-center p-1 rounded-lg">
+            <Icons slot="media" name="moon" className="mine-setting-icon" />
+            <div className="text-xs">绑定账号</div>
           </Link>
           <Link className="block m-0 text-center p-1 rounded-lg">
             <Icons slot="media" name="moon" className="mine-setting-icon" />
@@ -37,7 +41,26 @@ const Mine: React.FC = () => {
           </Link>
           <Link className="block m-0 text-center p-1 rounded-lg">
             <Icons slot="media" name="moon" className="mine-setting-icon" />
+            <div className="text-xs">预算</div>
+          </Link>
+          <Link className="block m-0 text-center p-1 rounded-lg">
+            <Icons slot="media" name="moon" className="mine-setting-icon" />
+            <div className="text-xs">资金计划</div>
+          </Link>
+          <Link className="block m-0 text-center p-1 rounded-lg">
+            <Icons slot="media" name="moon" className="mine-setting-icon" />
             <div className="text-xs">分类管理</div>
+          </Link>
+        </div>
+
+        <div className="bg-white p-3 grid gap-2 grid-cols-3 rounded-lg mx-4 mt-8">
+          <Link href="/about" className="block m-0 text-center p-1 rounded-lg">
+            <Icons slot="media" name="moon" className="mine-setting-icon" />
+            <div className="text-xs">关于</div>
+          </Link>
+          <Link href="#" className="block m-0 text-center p-1 rounded-lg">
+            <Icons slot="media" name="moon" className="mine-setting-icon" />
+            <div className="text-xs">退出</div>
           </Link>
         </div>
         {/* <List inset>
