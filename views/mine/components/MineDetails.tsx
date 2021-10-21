ad14f3d9ef929d4f17cf9e0007b59a905eea358f
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2021-10-21 23:29:14 +0800

    完善 mine page

--- views/mine/components/MineDetails.tsx
+++ views/mine/components/MineDetails.tsx
@@ -1,33 +1,26 @@
-import React, { memo, Fragment } from "react";
-import { BlockHeader } from "framework7-react";
-import { thousands } from "lib/api/utils";
-import Icons from "components/Icons";
+import React, { memo, Fragment, useEffect } from "react";
+import { BlockHeader, useStore } from "framework7-react";
 import { useUserQuery } from "apollo/graphql/model/user.graphql";
 import _ from "lodash";
+import Icons from "components/Icons";
+import { statisticsHandle } from "../tools";
+
 const MineDetails: React.FC = () => {
-  const { data, loading } = useUserQuery();
-  const statistics = [];
+  const token = useStore("token");
+  const { data, loading, refetch } = useUserQuery();
   const user = data?.user;
   const contrast = { days: "当天", months: "当月", years: "今年" };
+  const statistics = statisticsHandle(user?.statistics, contrast);
 
-  _.keys(user?.statistics).forEach((key) => {
-    if (key === "__typename") return;
-    const color = key === "pay" ? "text-green-500" : "text-red-700";
-    const title = key === "pay" ? "支出统计" : "收入统计";
-    const fields = { color, title };
-    const _details = user.statistics[key];
+  useEffect(() => {
+    token && refetch();
+  }, [token]);
 
-    _.keys(_details).forEach((dateKey) => {
-      if (dateKey === "__typename") return;
-      const type = key === "pay" ? "支出" : "收入";
-      fields[dateKey] = {
-        total: thousands(_details[dateKey]),
-        title: `${contrast[dateKey]}${type}`
-      };
-    });
-
-    statistics.push({ ...fields });
-  });
+  const hasLogged = !loading && token;
+  const accSkeleton =
+    !loading || (token && " skeleton-text skeleton-effect-wave");
+  const accOpacity = !token && " opacity-0";
+  const accClassName = `${accSkeleton}${accOpacity}`;
 
   return (
     <Fragment>
@@ -35,42 +28,39 @@ const MineDetails: React.FC = () => {
         <div className="mine-avatar w-20 h-20 rounded-full bg-gray-100 flex items-center justify-center overflow-hidden">
           <Icons name="moon" />
         </div>
-        {loading && (
-          <section className="text-center mt-3 skeleton-text skeleton-effect-wave">
-            <div className="text-xl font-bold">xxxxxxxx</div>
-            <div className="text-sm font-medium">xxxxxxxxxxxxxxxx</div>
-          </section>
-        )}
-        {!loading && (
-          <section className="text-center mt-3">
-            <div className="text-xl font-bold">{user?.username}</div>
-            <div className="text-sm font-medium">{user?.email}</div>
-          </section>
-        )}
+        <section className={`text-center mt-3${accClassName}`}>
+          <div className="text-xl font-bold">
+            {hasLogged ? user?.username : "xxxxxxxx"}
+          </div>
+          <div className="text-sm font-medium">
+            {hasLogged ? user?.email : "xxxxxxxxxxxxxxxx"}
+          </div>
+        </section>
       </div>
 
-      {statistics.map((item) => {
-        return (
-          <Fragment key={item.title}>
-            <BlockHeader className="mt-7 font-bold">{item.title}</BlockHeader>
-            <div className="mine-container p-2 mx-3 rounded-lg grid grid-cols-3 gap-2 mt-1 shadow-3">
-              {_.keys(contrast).map((key) => {
-                return (
-                  <section
-                    key={key}
-                    className={`rounded px-1 py-4 block my-0 text-center font-bold text-xs leading-loose shadow-2 ${item.color}`}
-                  >
-                    <div className="leading-none">{item[key].title}</div>
-                    <div className="truncate leading-none mt-2">
-                      {item[key].total}
-                    </div>
-                  </section>
-                );
-              })}
-            </div>
-          </Fragment>
-        );
-      })}
+      {token &&
+        statistics.map((item) => {
+          return (
+            <Fragment key={item.title}>
+              <BlockHeader className="mt-7 font-bold">{item.title}</BlockHeader>
+              <div className="mine-container p-2 mx-3 rounded-lg grid grid-cols-3 gap-2 mt-1 shadow-3">
+                {_.keys(contrast).map((key) => {
+                  return (
+                    <section
+                      key={key}
+                      className={`rounded px-1 py-4 block my-0 text-center font-bold text-xs leading-loose shadow-2 ${item.color}`}
+                    >
+                      <div className="leading-none">{item[key].title}</div>
+                      <div className="truncate leading-none mt-2">
+                        {item[key].total}
+                      </div>
+                    </section>
+                  );
+                })}
+              </div>
+            </Fragment>
+          );
+        })}
     </Fragment>
   );
 };
---
+++ views/mine/tools/index.ts
@@ -0,0 +1,26 @@
+import { thousands } from "lib/api/utils";
+import _ from "lodash";
+
+export const statisticsHandle = (data, contrast): any => {
+  const _statistics = [];
+  _.keys(data).forEach((key) => {
+    if (key === "__typename") return;
+    const color = key === "pay" ? "text-green-500" : "text-red-700";
+    const title = key === "pay" ? "支出统计" : "收入统计";
+    const fields = { color, title };
+    const _details = data[key];
+
+    _.keys(_details).forEach((dateKey) => {
+      if (dateKey === "__typename") return;
+      const type = key === "pay" ? "支出" : "收入";
+      fields[dateKey] = {
+        total: thousands(_details[dateKey]),
+        title: `${contrast[dateKey]}${type}`
+      };
+    });
+
+    _statistics.push({ ...fields });
+  });
+
+  return _statistics;
+};
