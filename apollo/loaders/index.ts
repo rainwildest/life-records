d4367c986e44af4831d9cfabe75d5d2f458aed53
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-10-21 17:52:47 +0800

    update

--- apollo/loaders/index.ts
+++ apollo/loaders/index.ts
@@ -1,11 +1,11 @@
 import DataLoader from "dataloader";
 import unique from "lib/api/unique";
-import { getUserByIdsQuery } from "db/sql/users";
+import { getUserByIds } from "db/sql/users";
 
 const createLoaders = {
   user: new DataLoader((keys) => {
     const ids = unique(keys);
-    return getUserByIdsQuery(ids).then((rows) =>
+    return getUserByIds(ids).then((rows) =>
       keys.map((key) => {
         return rows.find(
           (x: any) => x.id === key || x.seq_id.toString() === key
