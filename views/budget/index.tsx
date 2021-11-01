e173762a046d831abb0686bcda0757211cc7518b
DESKTOP-TMTGLP3\rainwildest <rainwildest@163.com>
2021-11-01 23:56:18 +0800

    添加 预算 页面

--- views/budget/index.tsx
+++ views/budget/index.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Page, Navbar } from "framework7-react";
+import Icons from "components/Icons";
 const Budget: React.FC = () => {
   return (
     <Page noToolbar>
       <Navbar backLink noHairline title="预算中心"></Navbar>
+
+      <div className="pt-2 px-6 mb-10 mt-6">
+        <div className="shadow-3 rounded-lg py-2 px-4 flex justify-between">
+          <div className="flex items-center flex-shrink-0 text-sm">
+            <Icons name="moon" />
+            生活费
+          </div>
+
+          <div
+            className="inset-shadow-3 rounded-lg"
+            style={{ minWidth: "50px" }}
+          ></div>
+        </div>
+      </div>
     </Page>
   );
 };
