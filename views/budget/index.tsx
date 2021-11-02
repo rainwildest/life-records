3c42d8a4427b92d3864d20ed5881b4a8538a5525
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-11-02 11:36:30 +0800

    完成预算界面的布局

--- views/budget/index.tsx
+++ views/budget/index.tsx
@@ -1,22 +1,39 @@
 import React from "react";
-import { Page, Navbar } from "framework7-react";
+import { Page, Navbar, BlockTitle } from "framework7-react";
 import Icons from "components/Icons";
+
 const Budget: React.FC = () => {
   return (
     <Page noToolbar>
       <Navbar backLink noHairline title="预算中心"></Navbar>
 
-      <div className="pt-2 px-6 mb-10 mt-6">
-        <div className="shadow-3 rounded-lg py-2 px-4 flex justify-between">
-          <div className="flex items-center flex-shrink-0 text-sm">
-            <Icons name="moon" />
-            生活费
+      <div className="pt-2 px-6 mb-10 mt-10">
+        <BlockTitle className="mx-0 mt-0 mb-7 text-gray-700 text-xl">
+          十月<span className="px-1">·</span>月预算
+        </BlockTitle>
+        <div className="shadow-3 rounded-lg py-3 px-4 flex justify-between">
+          <div className="budget-title flex items-center flex-shrink-0 text-sm">
+            <Icons name="budget" className="budget-icon pr-2" />
+            <div className="truncate text-gray-700">预算总金额</div>
           </div>
 
-          <div
-            className="inset-shadow-3 rounded-lg"
-            style={{ minWidth: "50px" }}
-          ></div>
+          <div className="shadow-3 rounded-lg w-24 h-10 flex items-center justify-center px-3 py-2 box-border text-sm overflow-hidden">
+            <div className="text-gray-700">100</div>
+          </div>
+        </div>
+
+        <div className="shadow-3 rounded-lg py-3 px-4 mt-10 flex justify-between">
+          <div className="budget-title flex items-center flex-shrink-0 text-sm">
+            <Icons name="moon" className="budget-icon pr-2" />
+            <div className="truncate text-gray-700">生活费</div>
+          </div>
+
+          <div className="inset-shadow-3 rounded-lg inline-block">
+            <input
+              className=" w-24 h-10 text-sm box-border px-3 py-2 text-center text-gray-700 bg-transparent"
+              placeholder="无预算"
+            />
+          </div>
         </div>
       </div>
     </Page>
