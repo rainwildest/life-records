956436664d12e37e015d7c6c0047cbc23d7294b7
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-11-06 09:31:50 +0800

    添加以及修改资金计划字段声明文件

--- typings/fund-plan.d.ts
+++ typings/fund-plan.d.ts
@@ -1,13 +1,17 @@
 declare type FundPlanOptions = {
   expenseId: string;
   userId: string;
+  name: string;
   amounts: number;
   approximateAt: Date;
+  completeAt: Date;
 };
 
 declare type FundPlanSnakeOptions = {
   expense_id: string;
   user_id: string;
+  name: string;
   amounts: number;
   approximate_at: Date;
+  complete_at: Date;
 };
