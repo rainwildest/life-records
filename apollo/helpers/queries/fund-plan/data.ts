ea71c5b7b487e114d463a5c19838d4c4bbf9f0d3
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-11-07 20:03:01 +0800

    update

--- apollo/helpers/queries/fund-plan/data.ts
+++ apollo/helpers/queries/fund-plan/data.ts
@@ -1,18 +1,45 @@
 import { AuthenticationError } from "apollo-server-micro";
-import { getPlannedByUserId } from "db/sql/fund-plan";
+import {
+  getPlannedByUserId,
+  getCompletedByUserId,
+  getOverdueByUserId
+} from "db/sql/fund-plan";
+
+const getCurrentDate = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const days = new Date(year, month + 1, 0);
+
+  return `${year}-${month + 1}-${days}`;
+};
 
 export default (
   _parent: unknown,
-  _args: { type: string },
+  _args: { type: string; date: string },
   context: unknown
 ): Promise<any> => {
   const { user } = context as GraphqlContext;
-
+  const { type, date } = _args;
   if (!user?.id) {
     throw new AuthenticationError(
       "Authentication token is invalid, please log in"
     );
   }
 
-  return getPlannedByUserId(user.id);
+  let _fun = null;
+
+  switch (type) {
+    case "overdue":
+      _fun = getOverdueByUserId(user.id, getCurrentDate());
+      break;
+    case "complete":
+      _fun = getCompletedByUserId(user.id, date);
+      break;
+    default:
+      _fun = getPlannedByUserId(user.id);
+      break;
+  }
+
+  return _fun;
 };
