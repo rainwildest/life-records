24a353de4dd315cc804f0031fa62e8594265fd32
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-11-09 18:11:30 +0800

    修改路由声明文件

--- typings/f7-route.d.ts
+++ typings/f7-route.d.ts
@@ -1,6 +1,11 @@
 import { Router } from "framework7/modules/router/router";
 
-export declare type RouterOpotions = {
-  f7route?: Router.Route;
+export declare type RouterOpotions = F7Router & F7Route;
+
+export declare type F7Router = {
   f7router?: Router.Router;
+};
+
+export declare type F7Route = {
+  f7route?: Router.Route;
 };
