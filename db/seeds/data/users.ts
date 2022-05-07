cda879f89d4db8b2e410f85a99eddbfecd1e9f4a
rainwildest <rainwildest@163.com>
2022-05-07 13:41:19 +0800

    添加 icons 名称

--- db/seeds/data/users.ts
+++ db/seeds/data/users.ts
@@ -8,6 +8,7 @@ export default async (knex: Knex, ctx: CtxOptions): Promise<void> => {
   const users = [
     {
       id: uuid(),
+      avatar: "avatar-05",
       username: "rainwildest",
       email: "rainwildest@163.com",
       password: "25d55ad283aa400af464c76d713c07ad"
